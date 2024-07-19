c0a55cc5dbe587eb1909512ec32c73f20b989293
milansrdic2000 <milansrdic2000@gmail.com>
2024-07-19 15:35:38 +0200

    added paths for db

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,21 @@
 const mySql = require("mysql");
 
+// const connection = mySql.createConnection({
+//   host: "127.0.0.1", // e.g., 'mysql.yourdomain.com'
+//   user: "root",
+//   password: "root",
+//   port: 3306,
+//   database: "rockettt_admin_panel",
+//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+// });
+
 const connection = mySql.createConnection({
-  host: "127.0.0.1", // e.g., 'mysql.yourdomain.com'
-  user: "root",
-  password: "root",
-  port: 3306,
+  host: "rockettt.rs", // e.g., 'mysql.yourdomain.com'
+  user: "rockettt_miki",
+  password: "fds57,Fk",
+  //   port: 3306,
   database: "rockettt_admin_panel",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+  //   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
 });
 
 module.exports = connection;
