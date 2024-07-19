01410b3e0e43942e28699dd59be754853f3bd4a6
milansrdic2000 <milansrdic2000@gmail.com>
2024-07-19 16:48:44 +0200

    change path test

--- src/upload/uploadController.js
+++ src/upload/uploadController.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "../../public_html/uploads/");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
