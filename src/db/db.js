b87d98d7975f1ae831c7434c2a979793fdc31383
milansrdic2000 <milansrdic2000@gmail.com>
2024-08-03 15:58:54 +0200

    Stupid cpanel and env remove

--- src/db/db.js
+++ src/db/db.js
@@ -1,24 +1,21 @@
 const mySql = require("mysql");
-require("dotenv").config();
 
-const prod = process.env.production === "true";
-console.log(prod);
-const connection = prod
-  ? mySql.createConnection({
-      host: "rockettt.rs", // e.g., 'mysql.yourdomain.com'
-      user: process.env.dbUser,
-      password: process.env.dbPass,
-      //   port: 3306,
-      database: "rockettt_admin_panel",
-      //   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-    })
-  : mySql.createConnection({
-      host: "127.0.0.1", // e.g., 'mysql.yourdomain.com'
-      user: "root",
-      password: "root",
-      port: 3306,
-      database: "rockettt_admin_panel",
-      socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-    });
+// const connection = mySql.createConnection({
+//   host: "127.0.0.1", // e.g., 'mysql.yourdomain.com'
+//   user: "root",
+//   password: "root",
+//   port: 3306,
+//   database: "rockettt_admin_panel",
+//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+// });
+
+const connection = mySql.createConnection({
+  host: "rockettt.rs", // e.g., 'mysql.yourdomain.com'
+  user: "rockettt_miki",
+  password: "fds57,Fk",
+  //   port: 3306,
+  database: "rockettt_admin_panel",
+  //   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+});
 
 module.exports = connection;
