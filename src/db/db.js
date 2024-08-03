a02d12c0b940f97d9bb0144fd4b77a11efa0732e
milansrdic2000 <milansrdic2000@gmail.com>
2024-08-03 14:55:09 +0200

    changed db

--- src/db/db.js
+++ src/db/db.js
@@ -1,21 +1,23 @@
 const mySql = require("mysql");
 
-const connection = mySql.createConnection({
-  host: "127.0.0.1", // e.g., 'mysql.yourdomain.com'
-  user: "root",
-  password: "root",
-  port: 3306,
-  database: "rockettt_admin_panel",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-});
+const prod = true;
 
-// const connection = mySql.createConnection({
-//   host: "rockettt.rs", // e.g., 'mysql.yourdomain.com'
-//   user: "rockettt_miki",
-//   password: "fds57,Fk",
-//   //   port: 3306,
-//   database: "rockettt_admin_panel",
-//   //   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-// });
+const connection = prod
+  ? mySql.createConnection({
+      host: "rockettt.rs", // e.g., 'mysql.yourdomain.com'
+      user: "rockettt_miki",
+      password: "fds57,Fk",
+      //   port: 3306,
+      database: "rockettt_admin_panel",
+      //   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+    })
+  : mySql.createConnection({
+      host: "127.0.0.1", // e.g., 'mysql.yourdomain.com'
+      user: "root",
+      password: "root",
+      port: 3306,
+      database: "rockettt_admin_panel",
+      socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+    });
 
 module.exports = connection;
